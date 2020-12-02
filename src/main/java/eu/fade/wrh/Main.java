2bbf97bd5aa73da5c28bd6a77d8951723148ee9d
franc088 <francis.debrabandere@tvh.com>
2020-12-02 11:31:28 +0100

    Solution lab3

--- src/main/java/eu/fade/wrh/Main.java
+++ src/main/java/eu/fade/wrh/Main.java
@@ -2,22 +2,67 @@ package eu.fade.wrh;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import eu.fade.wrh.domain.Detail;
 
 public class Main {
 
-    private void createTable() {
-        EntityManagerFactory emf;
-        EntityManager em;
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("warehouse");;
+    EntityManager em;
 
-        emf = Persistence
-                .createEntityManagerFactory("warehouse");
+    private void createAndUpdate() {
         em = emf.createEntityManager();
 
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        Detail detail = new Detail();
+        detail.setMaximumStock(10);
+        detail.setMinimumStock(2);
+        detail.setPrice(10.5);
+        em.persist(detail);
+        tx.commit();
+
+        Detail detailRetrieved = em.find(Detail.class, 1);
+        System.out.println(detailRetrieved.getId() + " - " +
+                                   detailRetrieved.getMaximumStock() + " - " +
+                                   detailRetrieved.getMinimumStock() + " - " +
+                                   detailRetrieved.getPrice());
+
+        tx.begin();
+        detailRetrieved.setMaximumStock(20);
+        em.persist(detailRetrieved);
+        tx.commit();
+
+        Detail detailUpdatedRetrieved = em.find(Detail.class, 1);
+        System.out.println(detailUpdatedRetrieved.getId() + " - " +
+                                   detailUpdatedRetrieved.getMaximumStock() + " - " +
+                                   detailUpdatedRetrieved.getMinimumStock() + " - " +
+                                   detailUpdatedRetrieved.getPrice());
+
+        em.close();
+        detailUpdatedRetrieved.setMaximumStock(99);
+
+    }
+
+    private void findDetail() {
+
+        em = emf.createEntityManager();
+        Detail detailRetrieved = em.find(Detail.class, 1);
+        System.out.println(detailRetrieved.getId() + " - " +
+                                   detailRetrieved.getMaximumStock() + " - " +
+                                   detailRetrieved.getMinimumStock() + " - " +
+                                   detailRetrieved.getPrice());
+
+        em.close();
     }
 
     public static void main(String[] args) {
         Main app = new Main();
-        app.createTable();
+        app.createAndUpdate();
+        app.findDetail();
+        app.emf.close();
     }
 }
---
+++ src/main/java/eu/fade/wrh/domain/Detail.java
@@ -0,0 +1,50 @@
+package eu.fade.wrh.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer minimumStock;
+    private Integer maximumStock;
+    private Double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMinimumStock() {
+        return minimumStock;
+    }
+
+    public void setMinimumStock(final Integer minimumStock) {
+        this.minimumStock = minimumStock;
+    }
+
+    public Integer getMaximumStock() {
+        return maximumStock;
+    }
+
+    public void setMaximumStock(final Integer maximumStock) {
+        this.maximumStock = maximumStock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(final Double price) {
+        this.price = price;
+    }
+}
