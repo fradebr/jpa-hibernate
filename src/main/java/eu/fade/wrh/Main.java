a92148e759ef9f754ffa7b5a9f7dc9e5e1579a30
franc088 <francis.debrabandere@tvh.com>
2020-12-02 10:19:41 +0100

    Solution Lab2

--- src/main/java/eu/fade/wrh/Main.java
+++ src/main/java/eu/fade/wrh/Main.java
@@ -1,8 +1,23 @@
 package eu.fade.wrh;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+
+    private void createTable() {
+        EntityManagerFactory emf;
+        EntityManager em;
+
+        emf = Persistence
+                .createEntityManagerFactory("warehouse");
+        em = emf.createEntityManager();
+
+    }
 
     public static void main(String[] args) {
         Main app = new Main();
+        app.createTable();
     }
 }
---
+++ src/main/java/eu/fade/wrh/domain/Item.java
@@ -0,0 +1,55 @@
+package eu.fade.wrh.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    private Integer id;
+    private String name;
+    private Integer currentStock;
+    private String type;
+    private String make;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Integer getCurrentStock() {
+        return currentStock;
+    }
+
+    public void setCurrentStock(final Integer currentStock) {
+        this.currentStock = currentStock;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(final String make) {
+        this.make = make;
+    }
+}
